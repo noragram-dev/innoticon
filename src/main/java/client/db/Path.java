93fdb4bd6b8cdfb0d99bcc5eb7bd369a1b08be6d
novemberizing <novemberizing@gmail.com>
2017-02-11 16:34:01 +0900

    default constructor

--- src/main/java/client/db/Path.java
+++ src/main/java/client/db/Path.java
@@ -7,31 +7,37 @@ package client.db;
  */
 @SuppressWarnings({"unused", "WeakerAccess"})
 public class Path {
-    public static String getUserRootPath(String uid){ return "/user/" + uid; }
+    public static String getUserRootPath(String uid) throws Exception {
+        if(uid==null){ throw new Exception("uid==null"); }
+        return "/user/" + uid;
+    }
 
-    public static String getUserClientRootPath(String uid){ return getUserRootPath(uid) + "/client"; }
-    public static String getUserClientPath(String uid, String device, String app){ return getUserClientRootPath(uid) + "/device/" + device + "/" + app; }
+    public static String getUserClientRootPath(String uid) throws Exception { return getUserRootPath(uid) + "/client"; }
+    public static String getUserClientPath(String uid, String device, String app) throws Exception {
+        if(device==null || app==null){ throw new Exception("device==null || app==null"); }
+        return getUserClientRootPath(uid) + "/device/" + device + "/" + app;
+    }
     
-    public static String getUserClientRequestRootPath(String uid){ return getUserClientRootPath(uid) + "/request"; }
-    public static String getUserClientResponseRootPath(String uid){ return getUserClientRootPath(uid) + "/response"; }
+    public static String getUserClientRequestRootPath(String uid) throws Exception { return getUserClientRootPath(uid) + "/request"; }
+    public static String getUserClientResponseRootPath(String uid) throws Exception { return getUserClientRootPath(uid) + "/response"; }
 
-    public static String getUserClientRequestPath(String uid, innoticon.ds.Req req){ return getUserClientRequestRootPath(uid) + "/" + req.action.key.c.hex(); }
+    public static String getUserClientRequestPath(String uid, innoticon.ds.Req req) throws Exception { return getUserClientRequestRootPath(uid) + "/" + req.action.key.c.hex(); }
 
-    public static String getUserClientResponsePath(String uid, innoticon.ds.Req req){ return getUserClientResponseRootPath(uid) + "/" + req.action.key.c.hex() + "/main"; }
-    public static String getUserClientResponsePath(String uid, innoticon.ds.Req req, innoticon.ds.Res.Key key){ return getUserClientRequestRootPath(uid) + "/" + req.action.key.c.hex() + "/" + req.action.key.s.hex() + "/" + key.hex(); }
+    public static String getUserClientResponsePath(String uid, innoticon.ds.Req req) throws Exception { return getUserClientResponseRootPath(uid) + "/" + req.action.key.c.hex() + "/main"; }
+    public static String getUserClientResponsePath(String uid, innoticon.ds.Req req, innoticon.ds.Res.Key key) throws Exception { return getUserClientRequestRootPath(uid) + "/" + req.action.key.c.hex() + "/" + req.action.key.s.hex() + "/" + key.hex(); }
 
-    public static String getUserProfileRootPath(String uid){ return getUserRootPath(uid) + "/profile"; }
-    public static String getUserProfileMainPath(String uid){ return getUserProfileRootPath(uid) + "/main"; }
-    public static String getUserProfileMainEmailPath(String uid){ return getUserProfileMainPath(uid) + "/email"; }
-    public static String getUserProfileMainPhonePath(String uid){ return getUserProfileMainPath(uid) + "/phone"; }
-    public static String getUserProfileMainNamePath(String uid){ return getUserProfileMainPath(uid) + "/phone"; }
-    public static String getUserProfilePath(String uid, String provider){ return getUserProfileRootPath(uid) + "/" + provider; }
+    public static String getUserProfileRootPath(String uid) throws Exception { return getUserRootPath(uid) + "/profile"; }
+    public static String getUserProfileMainPath(String uid) throws Exception { return getUserProfileRootPath(uid) + "/main"; }
+    public static String getUserProfileMainEmailPath(String uid) throws Exception { return getUserProfileMainPath(uid) + "/email"; }
+    public static String getUserProfileMainPhonePath(String uid) throws Exception { return getUserProfileMainPath(uid) + "/phone"; }
+    public static String getUserProfileMainNamePath(String uid) throws Exception { return getUserProfileMainPath(uid) + "/phone"; }
+    public static String getUserProfilePath(String uid, String provider) throws Exception { return getUserProfileRootPath(uid) + "/" + provider; }
 
-    public static String getUserInboxRootPath(String uid){ return getUserRootPath(uid) + "/inbox"; }
-    public static String getUserInvitationInboxRootPath(String uid){ return getUserInboxRootPath(uid) + "/invitation"; }
-    public static String getUserInvitationPath(String uid, String friend){ return getUserInvitationInboxRootPath(uid) + "/" + friend; }
-    public static String getUserMessageInboxRootPath(String uid){ return getUserInboxRootPath(uid) + "/message"; }
+    public static String getUserInboxRootPath(String uid) throws Exception { return getUserRootPath(uid) + "/inbox"; }
+    public static String getUserInvitationInboxRootPath(String uid) throws Exception { return getUserInboxRootPath(uid) + "/invitation"; }
+    public static String getUserInvitationPath(String uid, String friend) throws Exception { return getUserInvitationInboxRootPath(uid) + "/" + friend; }
+    public static String getUserMessageInboxRootPath(String uid) throws Exception { return getUserInboxRootPath(uid) + "/message"; }
 
-    public static String getUserFriendRootPath(String uid){ return getUserRootPath(uid) + "/friend"; }
-    public static String getUserFriendPath(String uid, String friend){ return getUserFriendRootPath(uid) + "/" + friend; }
+    public static String getUserFriendRootPath(String uid) throws Exception { return getUserRootPath(uid) + "/friend"; }
+    public static String getUserFriendPath(String uid, String friend) throws Exception { return getUserFriendRootPath(uid) + "/" + friend; }
 }
