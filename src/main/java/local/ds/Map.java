ac79175e80f38478dd13a7f9b305586e74f0e88a
novemberizing <novemberizing@gmail.com>
2017-02-12 11:21:27 +0900

    add once

--- src/main/java/local/ds/Map.java
+++ src/main/java/local/ds/Map.java
@@ -12,7 +12,10 @@ public class Map<K, V> implements local.Map<K, V> {
     protected final HashMap<K, V> __map = new HashMap<>();
 
     @Override public V get(K key) { return __map.get(key); }
-    @Override public V set(K key, V value) { return __map.put(key, value); }
+    @Override public V set(K key, V value) {
+        __map.put(key, value);
+        return value;
+    }
     @Override public V del(K key) { return __map.remove(key); }
     @Override public int size() { return __map.size(); }
 }
