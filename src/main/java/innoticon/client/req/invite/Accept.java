06d9abc4bc17dd79a9900150ad94a6184ca296df
novemberizing <novemberizing@gmail.com>
2017-02-12 10:30:10 +0900

    add once

--- src/main/java/innoticon/client/req/invite/Accept.java
+++ src/main/java/innoticon/client/req/invite/Accept.java
@@ -12,25 +12,66 @@ import static innoticon.ds.Action.Type.ACCEPTTOINVITEREQ;
 @SuppressWarnings({"DanglingJavadoc", "WeakerAccess", "unused"})
 public class Accept extends innoticon.ds.Req {
 
-    public static Accept Gen(String uid) {
+    public static Accept Gen(String uid, innoticon.ds.Res res) {
         innoticon.Client client = innoticon.Client.Get();
-        return new Accept(client.gen(), uid);
+        return new Accept(client.gen(), uid, res);
+    }
+
+    public static Accept Gen(String uid, innoticon.ds.Req req) {
+        innoticon.Client client = innoticon.Client.Get();
+        return new Accept(client.gen(), uid, req);
+    }
+
+    public static Accept Gen(String uid, innoticon.ds.Req req, String greeting) {
+        innoticon.Client client = innoticon.Client.Get();
+        return new Accept(client.gen(), uid, req, greeting);
     }
 
     @Expose public String uid;
-
-    public String v() { return uid; }
+    @Expose public innoticon.ds.User profile;
+    @Expose public innoticon.ds.Res res;
 
     public String friend(){ return uid; }
+    public innoticon.ds.User profile(){ return profile; }
+    public innoticon.ds.Res res(){ return res; }
 
     public Accept() { this.uid = null; }
 
-    public Accept(long unique, String uid) {
+    public Accept(long unique, String uid, innoticon.ds.Res res) {
         innoticon.Client client = innoticon.Client.Get();
 
         /** generate client key */
         action = client.genAction(ACCEPTTOINVITEREQ, client.key(), innoticon.ds.Action.Key.Gen(unique));
         /** detail */
         this.uid = uid;
+        this.profile = client.me();
+        this.res = res;
+    }
+
+    public Accept(long unique, String uid, innoticon.ds.Req req) {
+        innoticon.Client client = innoticon.Client.Get();
+
+        /** generate client key */
+        action = client.genAction(ACCEPTTOINVITEREQ, client.key(), innoticon.ds.Action.Key.Gen(unique));
+        /** detail */
+        this.uid = uid;
+        this.profile = client.me();
+        this.res = new innoticon.ds.Res(req.getClass(),
+                req.action(),
+                client.genResKey());
+    }
+
+    public Accept(long unique, String uid, innoticon.ds.Req req, String greeting) {
+        innoticon.Client client = innoticon.Client.Get();
+
+        /** generate client key */
+        action = client.genAction(ACCEPTTOINVITEREQ, client.key(), innoticon.ds.Action.Key.Gen(unique));
+        /** detail */
+        this.uid = uid;
+        this.profile = client.me();
+        this.res = new innoticon.ds.Res(req.getClass(),
+                greeting,
+                req.action(),
+                client.genResKey());
     }
 }
--- src/main/java/innoticon/client/req/invite/Reject.java
+++ src/main/java/innoticon/client/req/invite/Reject.java
@@ -13,25 +13,51 @@ import static innoticon.ds.Action.Type.REJECTTOINVITEREQ;
 @SuppressWarnings({"DanglingJavadoc", "WeakerAccess", "unused"})
 public class Reject extends innoticon.ds.Req {
 
-    public static Reject Gen(String uid) {
+    public static Reject Gen(String uid, innoticon.ds.Req req) {
         innoticon.Client client = innoticon.Client.Get();
-        return new Reject(client.gen(), uid);
+        return new Reject(client.gen(), uid, req);
+    }
+
+    public static Reject Gen(String uid, innoticon.ds.Req req, String greeting) {
+        innoticon.Client client = innoticon.Client.Get();
+        return new Reject(client.gen(), uid, req, greeting);
     }
 
     @Expose public String uid;
+    @Expose public innoticon.ds.Res res;
 
-    public String v() { return uid; }
-
+    public innoticon.ds.Res res(){ return res; }
     public String friend(){ return uid; }
 
     public Reject() { this.uid = null; }
 
-    public Reject(long unique, String uid) {
+    public Reject(long unique, String uid, innoticon.ds.Res res) {
         innoticon.Client client = innoticon.Client.Get();
 
         /** generate client key */
         action = client.genAction(REJECTTOINVITEREQ, client.key(), innoticon.ds.Action.Key.Gen(unique));
         /** detail */
         this.uid = uid;
+        this.res = res;
+    }
+
+    public Reject(long unique, String uid, innoticon.ds.Req req) {
+        innoticon.Client client = innoticon.Client.Get();
+
+        /** generate client key */
+        action = client.genAction(REJECTTOINVITEREQ, client.key(), innoticon.ds.Action.Key.Gen(unique));
+        /** detail */
+        this.uid = uid;
+        this.res = new innoticon.ds.Res(req.getClass(),req.action(),client.genResKey());
+    }
+
+    public Reject(long unique, String uid, innoticon.ds.Req req, String sorry) {
+        innoticon.Client client = innoticon.Client.Get();
+
+        /** generate client key */
+        action = client.genAction(REJECTTOINVITEREQ, client.key(), innoticon.ds.Action.Key.Gen(unique));
+        /** detail */
+        this.uid = uid;
+        this.res = new innoticon.ds.Res(req.getClass(), sorry, req.action(),client.genResKey());
     }
 }
