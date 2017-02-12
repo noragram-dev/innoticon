5820d9a14133275aff837f67c1abf337b2346425
novemberizing <novemberizing@gmail.com>
2017-02-12 17:31:21 +0900

    add once

--- src/main/java/innoticon/req/dialog/Create.java
+++ src/main/java/innoticon/req/dialog/Create.java
@@ -1,4 +1,4 @@
-package innoticon.dialog;
+package innoticon.req.dialog;
 
 import com.google.gson.annotations.Expose;
 import innoticon.req.Dialog;
@@ -16,13 +16,18 @@ import static innoticon.ds.Action.Type.DIALOG_CREATE_REQ;
  */
 public class Create extends Dialog {
 
-    public static innoticon.dialog.Create Gen(String title, String password){
+    public static innoticon.req.dialog.Create Gen(String title, String password){
         innoticon.Client client = innoticon.Client.Get();
         return new Create(client.gen(), title, password);
     }
 
+    public static innoticon.req.dialog.Create Gen(String title){
+        innoticon.Client client = innoticon.Client.Get();
+        return new Create(client.gen(), title);
+    }
+
     @Expose public String title;
-    @Expose public String password;
+    @Expose public String password;         /** currently not support */
     @Expose public HashMap<String,innoticon.ds.User> users;
 
     public String title(){ return title; }
@@ -52,6 +57,19 @@ public class Create extends Dialog {
         title = null;
         password = null;
         users = null;
+    }
+
+    public Create(long unique,String title){
+        innoticon.Client client = innoticon.Client.Get();
+
+        /** generate client key */
+        action = client.genAction(DIALOG_CREATE_REQ, client.key(), innoticon.ds.Action.Key.Gen(unique));
+
+        /** dialog key generate by server */
+
+        /** set detail */
+        this.title = title;
+        this.password = null;
     }
 
     public Create(long unique,String title, String password){
