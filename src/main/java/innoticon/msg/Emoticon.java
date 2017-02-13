b59e62ec6af56f9b894404dc3b13818bbf436434
novemberizing <novemberizing@gmail.com>
2017-02-13 18:33:11 +0900

    make modify buid fail

--- src/main/java/innoticon/msg/Emoticon.java
+++ src/main/java/innoticon/msg/Emoticon.java
@@ -7,6 +7,7 @@ import com.google.gson.annotations.Expose;
  */
 public class Emoticon extends innoticon.msg.Abstract {
     // Unique resource ID.
+
     @Expose
     public String resourceId = null;
     // Text description.
