fd968e938408ce28b68c93b7595bc294f3ca1694
novemberizing <novemberizing@gmail.com>
2017-02-14 18:09:08 +0900

    modify

--- src/main/java/firebase/db/Get.java
+++ src/main/java/firebase/db/Get.java
@@ -1,23 +1,32 @@
 package firebase.db;
 
 
-import com.google.firebase.database.*;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 
 /**
+ * remove get's deserializer
  *
  * @author novemberizing, me@novemberizing.net
  * @since 2017. 2. 14.
  */
-public class Get<T> extends novemberizing.rx.Operator<novemberizing.ds.tuple.Pair<String, novemberizing.ds.Deserializer<DataSnapshot,T>>, T> {
+@SuppressWarnings("WeakerAccess")
+public class Get<T> extends novemberizing.rx.Operator<String, T> {
+    protected GenericTypeIndicator<T> __indicator =  new GenericTypeIndicator<T>() {};
+
     @Override
-    protected void on(Task<novemberizing.ds.tuple.Pair<String, novemberizing.ds.Deserializer<DataSnapshot,T>>, T> task, novemberizing.ds.tuple.Pair<String, novemberizing.ds.Deserializer<DataSnapshot,T>> in) {
+    protected void on(Task<String, T> task, String in) {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference reference = database.getReference(in.first);
+        DatabaseReference reference = database.getReference(in);
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 try {
-                    task.next(in.second.deserialize(snapshot));
+                    task.next(snapshot.getValue(__indicator));
                 } catch(Exception e){
                     task.error(e);
                 } finally {
