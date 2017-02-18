ca6d65beaa2e87f8126e5e733ef6f4ef94d54a43
novemberizing <novemberizing@gmail.com>
2017-02-18 16:55:42 +0900

    implement get & set by index

---
+++ src/main/java/local/ds/ArrayHashMap.java
@@ -0,0 +1,31 @@
+package local.ds;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author novemberizing, me@novemberizing.net
+ * @since 2017. 2. 18.
+ */
+public class ArrayHashMap<K, V> extends local.ds.Map<K, V> {
+    protected ArrayList<K> __keys;
+
+    @Override public V set(K key, V value) {
+        __map.put(key, value);
+        __keys.add(key);
+        return value;
+    }
+    @Override public V del(K key) {
+        __keys.remove(key);
+        return __map.remove(key);
+    }
+
+    public K key(int i){
+        return (i>=0 && i<__keys.size()) ? __keys.get(i) : null;
+    }
+
+    public V value(int i){
+        K key = key(i);
+        return __map.get(key);
+    }
+}
--- src/main/java/local/ds/Map.java
+++ src/main/java/local/ds/Map.java
@@ -13,6 +13,7 @@ public class Map<K, V> implements local.Map<K, V> {
     @Override public V get(K key) { return __map.get(key); }
     @Override public V set(K key, V value) {
         __map.put(key, value);
+
         return value;
     }
     @Override public V del(K key) { return __map.remove(key); }
