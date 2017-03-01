9e4e194cb422e9a0afb3955c6f070ac701069026
novemberizing <me@novemberizing.net>
2017-03-02 07:42:30 +0900

    envelope res

--- src/main/java/innoticon/msg/Location.java
+++ src/main/java/innoticon/msg/Location.java
@@ -10,18 +10,26 @@ import com.google.gson.annotations.Expose;
 @SuppressWarnings({"WeakerAccess", "unused"})
 public class Location extends innoticon.msg.Text {
 
-    public static innoticon.msg.Text Gen(String str, String image){
-        return new innoticon.msg.Location(str, image);
+    public static innoticon.msg.Text Gen(String str, String image,int latitude,int longitude){
+        return new innoticon.msg.Location(str, image, latitude, longitude);
     }
 
     @Expose public String image = null;
+    @Expose public int latitude = 0;
+    @Expose public int longitude = 0;
 
     public String image(){ return image; }
     public void image(String v){ image = v; }
 
+    public void latitude(int v){ latitude = v; }
+    public int latitude(){ return latitude; }
+
+    public void longitude(int v){ longitude = v; }
+    public int longitude(){ return longitude; }
+
     public Location(){}
 
-    public Location(String str, String image){
+    public Location(String str, String image,int latitude,int longitude){
         this.key = new innoticon.ds.Message.Key(getClass().getName());
         this.str = str;
         this.image = image;
